3c81633a35200cfb2f543f5063f4361762bad719
Kulik <Krystian.Kulik@opitz-consulting.com>
2018-06-14 16:00:06 +0200

    po czesci dziala beanowanie akcji

--- src/main/java/com/opitz/form/SignUpForm.java
+++ src/main/java/com/opitz/form/SignUpForm.java
@@ -11,12 +11,15 @@ import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionMessage;
 import org.apache.struts.validator.ValidatorForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@Component
 public class SignUpForm extends ValidatorForm {
 
     private String username;
@@ -24,9 +27,12 @@ public class SignUpForm extends ValidatorForm {
     private String password;
     private String confirmPassword;
 
+    @Autowired
+    ClaimService claimService;
+
     @Override
     public ActionErrors validate(ActionMapping mapping, HttpServletRequest request) {
-        ClaimService claimService = ServiceLocator.findClaimService();
+//        ClaimService claimService = ServiceLocator.findClaimService();
 
         ActionErrors errors = super.validate(mapping, request);
         if (!StringUtils.equals(password, confirmPassword)) {
